18c40e48a55db44a04cfa2f98ee2861d051e18e5
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2017-09-30 08:08:39 +0300

    [Chore #151428835] Add application root prefix

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from app import create_app
 
 config_name = os.getenv('APP_SETTINGS')  # config_name = "development"
 app = create_app(config_name)
+app.config['APPLICATION_ROOT'] = '/v1'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
