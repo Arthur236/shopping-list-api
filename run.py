5d621b736a3fa12506322be7e1dd003b360430b6
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2017-09-30 07:57:49 +0300

    [Chore #151428835] Add port bindings to run.py for heroku

--- run.py
+++ run.py
@@ -2,11 +2,11 @@
 Running the application
 """
 import os
-
 from app import create_app
 
 config_name = os.getenv('APP_SETTINGS')  # config_name = "development"
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run('', port=port)
