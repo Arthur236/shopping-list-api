0e4c62854df99d43e167aabf64c2de318e15afba
Arthur236 <arthur.thungu@gmail.com>
2017-11-16 22:25:12 +0300

    [Chore] Add support for CORS

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 Initialing the application
 """
 from flask_api import FlaskAPI
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask import request, jsonify, redirect
 
@@ -40,6 +41,8 @@ def create_app(config_name):
     from .models import User
 
     app = FlaskAPI(__name__, instance_relative_config=True)
+    CORS(app)
+
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
